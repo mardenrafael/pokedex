cb8ead07bf2e4a0b027f4a2a52847c35d2b28ec4
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-07-25 20:36:50 -0400

    Busca por 3 pokemons aleatorios concluida

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon/pikachu"
+const BASE_URL = "https://pokeapi.co/api/v2/"
 
 
 
-
+//No momento em que o arquivo é carregado ele busca 3 pokemons aleatorios e insere ele na pagina
 async function get_random_pokemon() {
-    const response = await fetch(BASE_URL)
-    const json = await response.json()
+    const POKEMONS_URL = BASE_URL + "pokemon?offset=0&limit=20"
+    const response = await fetch(POKEMONS_URL)
+    const pokemon_list = await response.json().then((resp) => {
+        return resp.results
+    })
 
-    return json
+    let chossen_pokemons = []
+    
+    for(let i = 0; i < 3; i++) {
+        chossen_pokemons.push(pokemon_list[Math.floor(Math.random() * pokemon_list.length)])
+    }
+    
+    for (let i = 0; i < chossen_pokemons.length; i++) {
+        insert_pokemon_on_card(chossen_pokemons[i])
+    }
+
+    // const chossen_pokemon_json = await response.json()
+
+    // return chossen_pokemon_json
 }
 
 
-// recebe commo parametro o pokemon pesquisado
+// recebe commo parametro o pokemon pesquisado e insere ele na pagina
 function insert_pokemon_on_card(pokemon) {
     //limpar os dados do json
     //inserir eles na div de acordo com o design
+
+    console.log(pokemon.name);
 }
 
 
-get_random_pokemon().then( pokemon => { 
-    insert_pokemon_on_card(pokemon)
-})
+get_random_pokemon()
