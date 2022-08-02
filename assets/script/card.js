d34ede00a8347180b4103e30b0e62a43656c2282
Marden Rafael <mardenrafaeldalmagro.gimenez@gmail.com>
2022-08-01 23:42:45 -0400

    start abilities section

--- assets/script/card.js
+++ assets/script/card.js
@@ -1,81 +1,110 @@
 class CardCreator {
 
-    generate_card(pokemon_data) {
-        //Cria a div principal e configura ela
-        const div = document.createElement("div")
-        div.setAttribute("class", "card")
+  generate_card(pokemon_data) {
+    //Cria a div principal e configura ela
+    const div = document.createElement("div")
+    div.setAttribute("class", "card")
 
-        //Cria a header principal e configura ela
-        const header = document.createElement("header")
-        header.setAttribute("class", "header_card")
+    //Cria a header principal e configura ela
+    const header = document.createElement("header")
+    header.setAttribute("class", "header_card")
 
-        header.innerText = `${pokemon_data.name.toUpperCase()}`
+    header.innerText = `${pokemon_data.name.toUpperCase()}`
 
-        //Cria a section principal e configura ela
-        // const section = document.createElement("section")
-        // section.setAttribute("class", "section_card")
+    //Cria a section principal e configura ela
+    // const section = document.createElement("section")
+    // section.setAttribute("class", "section_card")
 
-        //Cria a imagem dos pokemons
-        const img_element = document.createElement("img")
-        img_element.setAttribute("src", pokemon_data.sprites.front_default)
-        img_element.setAttribute("alt", pokemon_data.name)
-        img_element.setAttribute("class", "card_img")
+    //Cria a imagem dos pokemons
+    const img_element = document.createElement("img")
+    img_element.setAttribute("src", pokemon_data.sprites.front_default)
+    img_element.setAttribute("alt", pokemon_data.name)
+    img_element.setAttribute("class", "card_img")
 
-        //Cria a footer principal e configura ela
-        const footer = document.createElement("footer")
-        footer.setAttribute("class", "footer_card")
+    //Cria a footer principal e configura ela
+    const footer = document.createElement("footer")
+    footer.setAttribute("class", "footer_card")
 
-        const more_info_button = document.createElement("button")
-        more_info_button.setAttribute("class", "more_info_btn")
+    const more_info_button = document.createElement("button")
+    more_info_button.setAttribute("class", "more_info_btn")
 
-        more_info_button.innerText = `Mais informações sobre ${pokemon_data.name}`
-       
-        // chama o método show_more_info
-        more_info_button.addEventListener("click", () => {
-            this.show_more_info(pokemon_data)
-        })
+    more_info_button.innerText = `Mais informações sobre ${pokemon_data.name}`
 
-        footer.appendChild(more_info_button)
+    // chama o método show_more_info
+    more_info_button.addEventListener("click", () => {
+      this.show_more_info(pokemon_data)
+    })
 
-        div.appendChild(header)
-        div.appendChild(img_element)
-        div.appendChild(footer)
+    footer.appendChild(more_info_button)
+
+    div.appendChild(header)
+    div.appendChild(img_element)
+    div.appendChild(footer)
 
 
-        return div
-    }
+    return div
+  }
 
-    show_more_info(pokemon_name) {
-        //seleciona a section onde as informações vão ser exibidas
-        const info_section = document.getElementById("show-info")
+  show_more_info(pokemon_data) {
+    //seleciona a section onde as informações vão ser exibidas
+    const info_section = document.getElementById("show-info")
 
-        //cria a div onde vão ser mostradas as informações
-        const div = document.createElement("div")
-        div.setAttribute("class", "info-card")
+    //cria a div onde vão ser mostradas as informações
+    const div = document.createElement("div")
+    div.setAttribute("class", "info-card")
 
-        //cria o header da div
-        const header = document.createElement("header")
-        header.setAttribute("class", "header_info")
-        header.innerText = `${pokemon_name.name.toUpperCase()}`
+    //cria o header da div
+    const header = document.createElement("header")
+    header.setAttribute("class", "header_info")
+    header.innerText = `${pokemon_data.name.toUpperCase()}`
 
-        //cria o elemento da imagem do pokémon
-        const img_element = document.createElement("img")
-        img_element.setAttribute("src", pokemon_name.sprites.front_default)
-        img_element.setAttribute("alt", pokemon_name.name)
-        img_element.setAttribute("class", "info_img")
+    //cria o elemento da imagem do pokémon
+    const img_element = document.createElement("img")
+    img_element.setAttribute("src", pokemon_data.sprites.front_default)
+    img_element.setAttribute("alt", pokemon_data.name)
+    img_element.setAttribute("class", "info_img")
 
-        const section = document.createElement("section")
-        section.setAttribute("class", "section_info")
+    const section = document.createElement("section")
+    section.setAttribute("class", "section_info")
 
-        section.appendChild(header)
-        section.appendChild(img_element)
+    section.appendChild(header)
+    section.appendChild(img_element)
 
-        div.appendChild(section)
+    //cria e configura a sections das abilidades do pokemon
+    const abilties_section = document.createElement("section")
+    abilties_section.setAttribute("class", "section_info")
 
-        //insere a div no html
-        info_section.replaceChildren(div)
-        
-        return div
+    //cria, configura e adiciona o cabeçalho da sections
+    const header_abilities = document.createElement("header")
+    header_abilities.setAttribute("class", "header_info")
+    header_abilities.innerText = `${pokemon_data.name.toUpperCase()} ABILITIES`
 
-    }
+    //cria, e configura um container para a lista de abilidades
+    const abilities_list_outter_container = document.createElement("section")
+    abilities_list_outter_container.setAttribute("class", "info_abilities")
+    //cria uma lista de abilidades
+    const abilities_list = document.createElement("ul")
+
+    pokemon_data.abilities.map((acc) => {
+      console.log(acc.is_hidden ? `${acc.ability.name} is hidden ability` : `${acc.ability.name} is not hidden ability`);
+    })
+
+    //insere a lista de abilidades dentro do container
+    abilities_list_outter_container.appendChild(abilities_list)
+
+
+    abilties_section.appendChild(header_abilities)
+    abilties_section.appendChild(abilities_list_outter_container)
+    //
+
+    div.appendChild(section)
+    div.appendChild(abilties_section)
+
+
+    //insere a div no html
+    info_section.replaceChildren(div)
+
+    return div
+
+  }
 }
